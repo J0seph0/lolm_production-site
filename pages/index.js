72a11f66a332c6aba5aa1e807afd504eb0393628
J0seph0 <jhoward26@ewu.edu>
2023-07-24 00:28:56 -0700

    tryed to add a nav bar and butt

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,14 @@ export default function Home() {
   return (
   <div className="container">
 
+
     <Head>
+      <script src="https://kit.fontawesome.com/8eb4682278.js" crossorigin="anonymous"></script>
       <title>Round Glass Media</title>
       <link rel="icon" href="/eyeglass.png" />
     </Head>
+
+
     <NavBar />
 
   </div>
