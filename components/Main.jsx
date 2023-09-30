ad3a66e43b87d075b95de69cbc6fb1fcd24f5b93
J0seph0 <jhoward26@ewu.edu>
2023-09-30 14:49:46 -0700

    postioned the main page and deleted pages for buil

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,14 +8,16 @@ const Main = () => {
 
   return (
     <div id='home' className='w-screen h-screen text-center'>
-      <div className='max-w-[1240px] w-full h-full'>
-          <div>
+      <div className='max-w-[1240px] w-full h-full flex'>
+          <div className='py-[100px] px-[50px]'>
             <h1 className='text-[100px]'>Round</h1>
             <h1 className='text-[100px]'>Glass</h1>
             <h1 className='text-[100px]'>Media</h1>
           </div>
-          <div>
+          <div className=''>
+            <p>
             Explain text
+            </p>
           </div>
       </div>
     </div>
