bd9826461a2d408d2bc7d24e9d1163c07cffc589
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2023-10-07 13:02:02 -0700

    updated the main page

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import Draggable from 'react-draggable'
-import Eyeglass from './Eyeglass'
-import video from '/public/roundglass_logo.mp4'
+import React from "react";
+import Draggable from "react-draggable";
+import Eyeglass from "./Eyeglass";
 
 const Main = () => {
-
-
-
   return (
-    <div id='home' className='w-screen h-screen text-center'>
-      <div className='max-w-[1240px] w-full h-full flex'>
-      <video>
-        <source src="/public/roundglass_logo.mp4" />
-      </video>
+    <div id="home" className="w-screen h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full flex">
+        <div className="justify-center">
+        <iframe
+          src="https://player.cloudinary.com/embed/?public_id=lhbum02rgwk86q1xwbg6&cloud_name=dpjdta2yo&player[controls]=false&player[muted]=true&player[hideContextMenu]=false&player[floatingWhenNotVisible]=false&player[autoplay]=true"
+          allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+          allowfullscreen
+          width='1920'
+          height='1080'
+          frameborder="0"
+          className=""
+          id="logoiframe"
+        ></iframe>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
