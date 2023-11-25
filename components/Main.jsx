e82e83ba227668945f23996ea6f10d7c429e4d96
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2023-11-25 14:53:47 -0800

    fixed the lottie fps

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
 <div id="home" className="w-screen h-fit md:h-screen flex items-center justify-center bg-[#ffffff]">
       <div className="max-w-[1240px] w-full h-full flex justify-center items-center">
       <Player
-        src='https://lottie.host/b6409945-8da7-4c9d-be75-eb2f49127681/aHjFQmoWNt.json'
+        src='https://lottie.host/ba5cef5a-9603-4c02-b7db-90e4fc09c388/4zRrgSc7mt.json'
         className="player w-full h-full m-auto"
         autoplay
         keepLastFrame='true'
