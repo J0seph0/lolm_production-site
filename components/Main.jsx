bc9f7c8d0680d0f3e9a7fb50ebe537418f453150
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2023-11-25 14:55:46 -0800

    main fit _-> screen

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,7 @@ import { Player } from '@lottiefiles/react-lottie-player';
 
 const Main = () => {
   return (
-<div id="home" className="w-screen h-fit md:h-screen flex items-center justify-center bg-[#ffffff]">
+<div id="home" className="w-screen h-screen md:h-screen flex items-center justify-center bg-[#ffffff]">
       <div className="max-w-[1240px] w-full h-full flex justify-center items-center">
       <Player
         src='https://lottie.host/ba5cef5a-9603-4c02-b7db-90e4fc09c388/4zRrgSc7mt.json'
