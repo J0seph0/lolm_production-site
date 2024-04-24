aac4f75526add663e15389ea0282043d3c1d0836
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2024-04-23 21:29:26 -0700

    Modified the works page a bit

--- pages/works.js
+++ pages/works.js
@@ -1,15 +1,43 @@
 import React from "react";
+import NavBar from "@components/NavBar";
 
 const works = () => {
+  const videos = [
+    {
+      title: "Work 1",
+      description: "Description of work 1",
+      url: "https://player.vimeo.com/video/866193669",
+    },
+    {
+      title: "Work 2",
+      description: "Description of work 2",
+      url: "https://player.vimeo.com/video/896365954",
+    },
+    {
+      title: "Work 2",
+      description: "Description of work 2",
+      url: "https://player.vimeo.com/video/918728388",
+    },
+    {
+      title: "Work 2",
+      description: "Description of work 2",
+      url: "https://player.vimeo.com/video/778298765",
+    },
+  ];
+
   return (
-    <div className="w-screen h-full p-5">
-      <h1 className="text-4xl font-bold mb-5">Our Works</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {/* Replace this with your actual data */}
-        {Array(9).fill().map((_, i) => (
-          <div key={i} className="border rounded-lg p-3">
-            <h2 className="font-semibold text-xl mb-2">Work {i + 1}</h2>
-            <p className="text-gray-500">Description of work {i + 1}</p>
+    <div className="w-screen h-screen grid">
+      <NavBar />
+      <div className="mt-20 h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {videos.map((video, i) => (
+          <div key={i} className="p-0 w-full h-full">
+            <iframe
+              className="w-full h-full"
+              src={video.url}
+              width="100%"
+              height="100%"
+              allow="autoplay; fullscreen; picture-in-picture"
+            />
           </div>
         ))}
       </div>
