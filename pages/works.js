87b3135aa0ef83976b941596f10530572be63514
J0seph0 <103216770+J0seph0@users.noreply.github.com>
2024-07-25 21:28:26 -0700

    Fixed horizontal scrolling

--- pages/works.js
+++ pages/works.js
@@ -26,23 +26,21 @@ const works = () => {
   ];
 
   return (
-    <div className="w-screen h-screen grid">
+    <div className="w-full h-full grid">
       <NavBar isDifferentPage={true}/>
-      <div className="mt-20 h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className=" mt-20 h-screen w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-3">
         <div className="flex flex-col items-center justify-center">
-        <h1 className="text-7xl text-[#37355A] font-bold w-fit mr-20">W</h1>
-        <h1 className="text-7xl text-[#37355A] font-bold w-fit mr-16">O</h1>
-        <h1 className="text-7xl text-[#37355A] font-bold w-fit mr-12">R</h1>
-        <h1 className="text-7xl text-[#37355A] font-bold w-fit mr-8">k</h1>
-        <h1 className="text-7xl text-[#37355A] font-bold w-fit mr-4">S</h1>
+        <h1 className="text-7xl text-[#37355A] font-bold w-fit relative" style={{ left: '0px' }}>W</h1>
+        <h1 className="text-7xl text-[#37355A] font-bold w-fit relative" style={{ left: '20px' }}>O</h1>
+        <h1 className="text-7xl text-[#37355A] font-bold w-fit relative" style={{ left: '40px' }}>R</h1>
+        <h1 className="text-7xl text-[#37355A] font-bold w-fit relative" style={{ left: '60px' }}>K</h1>
+        <h1 className="text-7xl text-[#37355A] font-bold w-fit relative" style={{ left: '80px' }}>S</h1>
         </div>
         {videos.map((video, i) => (
           <div key={i} className="p-0 w-full h-full">
             <iframe
-              className="w-full h-full"
+              className="w-full h-full rounded-sm"
               src={video.url}
-              width="100%"
-              height="100%"
               allow="autoplay; fullscreen; picture-in-picture"
             />
           </div>
